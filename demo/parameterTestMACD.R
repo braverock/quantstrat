f20cf2d9ad2283b306743cc8b67a7815dcd7864f
Yu Chen <ychen31@uic.edu>
2011-07-20 21:26:22 +0000

    Add comments Try to implement parallel execution  in applyParameter , but still need to solve the error.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/quantstrat@687 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- demo/parameterTestMACD.R
+++ demo/parameterTestMACD.R
@@ -3,7 +3,7 @@
 # Author: CCD
 ###############################################################################
 
-
+source("D:\\Documents\\GoogleCode\\Workspace\\quantstrat\\R\\parameters.R")
 
 #please run macd demo before all these...
 paramStructure<-getParameterTable(stratMACD)
@@ -19,6 +19,24 @@ tPD2<-setParameterDistribution(tPD2,'signal',indexnum=1,distribution=list(relati
 
 pConstraint2<-setParameterConstraint(constraintLabel='macdPC',paramList=c('nFast','nSlow'),relationship='lt')
 
-testPackList2<-applyParameter(strategy=stratMACD,portfolios=portfolio.st,parameterPool=tPD2,method='random',sampleSize=3,parameterConstrains=pConstraint2)
+
+
+#testPackList2<-applyParameter(strategy=stratMACD,portfolios=portfolio.st,parameterPool=tPD2,method='random',sampleSize=3,parameterConstrains=pConstraint2)
+#system.time(testPackList2<-applyParameter(strategy=stratMACD,portfolios=portfolio.st,parameterPool=tPD2,method='random',sampleSize=3,parameterConstrains=pConstraint2))
+
+require(foreach)
+require(doSMP)
+#
+#workers <- startWorkers(2) # My computer has 2 cores
+#registerDoSMP(workers)
+
+laststpar<-system.time(testPackList3<-applyParameter(strategy=stratMACD,portfolios=portfolio.st,parameterPool=tPD2,method='random',sampleSize=2,parameterConstrains=pConstraint2))
+laststpar
+#
+#stopWorkers(workers)
+#rmSessions(all=TRUE)
+
+
+
 
 
