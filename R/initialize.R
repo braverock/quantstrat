2268bd770c42dddfdf82904e50d7a02f245ce673
Brian G. Peterson <brian@braverock.com>
2011-08-06 14:45:03 +0000

    - stub file for common initialization of strategies
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/quantstrat@710 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/initialize.R
@@ -0,0 +1,19 @@
+###############################################################################
+# 
+# File for initialization (and possibly wrap-up) of strategy runs
+#
+###############################################################################
+
+
+###############################################################################
+# R (http://r-project.org/) Quantitative Strategy Model Framework
+#
+# Copyright (c) 2009-2011
+# Peter Carl, Dirk Eddelbuettel, Brian G. Peterson, Jeffrey Ryan, and Joshua Ulrich 
+#
+# This library is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file COPYING
+#
+# $Id$
+#
+###############################################################################
