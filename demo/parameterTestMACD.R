6d5ea65d2a55fc075fae96b1f8261625f320fdc1
Yu Chen <ychen31@uic.edu>
2011-08-12 20:22:58 +0000

    Some miner changes on demo.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/quantstrat@729 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- demo/parameterTestMACD.R
+++ demo/parameterTestMACD.R
@@ -8,21 +8,26 @@ require(doSMP)
 workers <- startWorkers(2)
 registerDoSMP(workers)
 
-
-source("D:\\Documents\\GoogleCode\\Workspace\\quantstrat\\R\\parameters.R")
-
 #please run macd demo before all these...
 paramStructure<-getParameterTable(stratMACD)
 
-tPD2<-setParameterDistribution() 
-tPD2
+rm(tPD2)
+
 
 # Just provide leagal values and use random sampling.
-tPD2<-setParameterDistribution(tPD2,'indicator',indexnum=1,distribution=list(nFast=(10:30)),label='nFast')
-#make them over lap from 20 to 30 to test.
-tPD2<-setParameterDistribution(tPD2,'indicator',indexnum=1,distribution=list(nSlow=(20:40)),label='nSlow')
-tPD2<-setParameterDistribution(tPD2,'signal',indexnum=1,distribution=list(relationship=c('gt','gte')),label='sig1.gtgte')
+# Make nFast and nSlow over lap from 20 to 30 to test the constrains later.
+# First call don't need to input tPD2, the funciton will initial one and return it.
 
+tPD2<-setParameterDistribution(type='indicator',indexnum=1,distribution=list(nFast=(10:30)),label='nFast')
+
+# Initial one tPD2 and pass it also works as following two lines:
+## tPD2<-setParameterDistribution() 
+## tPD2<-setParameterDistribution(type='indicator',indexnum=1,distribution=list(nFast=(10:30)),label='nFast')
+
+tPD2<-setParameterDistribution(tPD2,type='indicator',indexnum=1,distribution=list(nSlow=(20:40)),label='nSlow')
+tPD2<-setParameterDistribution(tPD2,type='signal',indexnum=1,distribution=list(relationship=c('gt','gte')),label='sig1.gtgte')
+
+# Put constrains to the nFast and nSlow parameters.
 pConstraint2<-setParameterConstraint(constraintLabel='macdPC',paramList=c('nFast','nSlow'),relationship='lt')
 
 
@@ -31,10 +36,17 @@ pConstraint2<-setParameterConstraint(constraintLabel='macdPC',paramList=c('nFast
 #system.time(testPackList2<-applyParameter(strategy=stratMACD,portfolios=portfolio.st,parameterPool=tPD2,method='random',sampleSize=3,parameterConstrains=pConstraint2))
 
 
-laststpar<-system.time(
-		testPackListPL<-applyParameter(strategy=stratMACD,portfolios=portfolio.st,parameterPool=tPD2,method='random',sampleSize=10,parameterConstrains=pConstraint2)
+laststpar.rnd<-system.time(
+		testPackListPL<-applyParameter(strategy=stratMACD,portfolios=portfolio.st,parameterPool=tPD2,method='random',sampleSize=20,parameterConstrains=pConstraint2)
 )
-laststpar
+laststpar.rnd
+
+# Please run either random mode or expand mode, since the latter run will overwrite the objects in .blotter.
+#laststpar.exp<-system.time(
+#		testPackListPL<-applyParameter(strategy=stratMACD,portfolios=portfolio.st,parameterPool=tPD2,method='random',sampleSize=20,parameterConstrains=pConstraint2)
+#)
+#laststpar.exp
+
 #
 #stopWorkers(workers)
 #rmSessions(all=TRUE)
