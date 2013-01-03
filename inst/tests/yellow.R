ce05ec26c5a36f4c8f131c192266c721ee297f41
Dan Wlasiuk <milktrader@gmail.com>
2013-01-03 04:10:44 +0000

    added 2 suites of tests for yellow, removed aqua and test_book
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/quantstrat@1335 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/tests/yellow.R
+++ inst/tests/yellow.R
@@ -1,13 +1,12 @@
 #!/usr/bin/Rscript --vanilla
 #
-# aqua.R
+# yellow.R
 #
-# this produces trade statistics that can be compared 
-# with results from other frameworks
+# long only simple moving average crossover
 #
-# the data is SPX daily data from 1/1/1970 to 12/31/1972
+# SPX daily data is from 1/1/1970 to 12/31/1972
 #
-# copyright (c) 2009-2012, Algorithm Alpha, LLC
+# copyright (c) 2009-2013, Algorithm Alpha, LLC
 # Licensed GPL-2
 #
 ################### LOAD QUANTSTRAT #################
@@ -20,8 +19,8 @@ data(spx)
 
 ############################# DEFINE VARIABLES ##############################
 
-port          = 'aquaPort'
-acct          = 'aquaAcct'
+port          = 'yellowPort'
+acct          = 'yellowAcct'
 initEq        = 1e6
 initDate      = '1969-12-31'
 fast          = 10 
@@ -34,19 +33,19 @@ stock('spx' ,currency='USD', multiplier=1)
 initPortf(port, 'spx', initDate=initDate)
 initAcct(acct, port, initEq=initEq, initDate=initDate)
 initOrders(port, initDate=initDate )
-aqua = strategy(port)
+yellow = strategy(port)
 
 ############################# INDICATORS ####################################
 
-aqua <- add.indicator(
-                     strategy  = aqua, 
+yellow <- add.indicator(
+                     strategy  = yellow, 
                      name      = 'SMA', 
                      arguments = list(x=quote(Cl(mktdata)), 
                                       n=fast),
                      label     = 'fast' )
 
-aqua <- add.indicator(
-                     strategy  = aqua, 
+yellow <- add.indicator(
+                     strategy  = yellow, 
                      name      = 'SMA', 
                      arguments = list(x=quote(Cl(mktdata)), 
                                       n=slow),
@@ -54,15 +53,15 @@ aqua <- add.indicator(
 
 ############################# SIGNALS #######################################
 
-aqua <- add.signal(
-                  strategy  = aqua,
+yellow <- add.signal(
+                  strategy  = yellow,
                   name      = 'sigCrossover',
                   arguments = list(columns=c('fast','slow'), 
                                    relationship='lt'),
                   label     = 'fast.lt.slow')
 
-aqua <- add.signal(
-                  strategy  = aqua,
+yellow <- add.signal(
+                  strategy  = yellow,
                   name      = 'sigCrossover',
                   arguments = list(columns=c('fast','slow'),
                                    relationship='gt'),
@@ -70,8 +69,8 @@ aqua <- add.signal(
 
 ############################# RULES #########################################
 
-aqua <- add.rule(
-                strategy  = aqua,
+yellow <- add.rule(
+                strategy  = yellow,
                 name      = 'ruleSignal',
                 arguments = list(sigcol    = 'fast.gt.slow',
                                  sigval    = TRUE,
@@ -81,8 +80,8 @@ aqua <- add.rule(
 
                 type      = 'enter',
                 label     = 'EnterLONG')
-aqua <- add.rule(
-                strategy  = aqua,
+yellow <- add.rule(
+                strategy  = yellow,
                 name      = 'ruleSignal',
                 arguments = list(sigcol    = 'fast.lt.slow',
                                  sigval    = TRUE,
@@ -94,7 +93,7 @@ aqua <- add.rule(
 
 ############################# APPLY STRATEGY ################################
 
-applyStrategy(aqua, port, prefer='Open', verbose=FALSE)
+applyStrategy(yellow, port, prefer='Open', verbose=FALSE)
 
 ############################# UPDATE ########################################
 
@@ -103,4 +102,4 @@ updatePortf(port, 'spx', Date=paste('::',as.Date(Sys.time()),sep=''))
 ########################### CONTAINERS CALLED IN TESTING #####################
 
 book = getOrderBook(port)
-
+stats = tradeStats(port)
