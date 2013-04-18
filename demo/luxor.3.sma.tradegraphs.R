c36ad3ad6326c5a4a42cf9f5762165c9712a23c9
Jan Humme <jan@opentrades.nl>
2013-04-18 20:34:27 +0000

    - adjusted more luxor demo code
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/quantstrat@1439 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- demo/luxor.3.sma.tradegraphs.R
+++ demo/luxor.3.sma.tradegraphs.R
@@ -1,8 +1,18 @@
 #!/usr/bin/Rscript --vanilla
+#
+# Jan Humme (@opentrades) - August 2012, revised April 2013
+#
+# Tested and found to work correctly using blotter r1420
+#
+# From Jaekle & Tamasini: A new approach to system development and portfolio optimisation (ISBN 978-1-905641-79-6)
 
 require(quantstrat)
 
+### load 'stats' back into .GlobalEnv
+
 load('../data/luxor.parameters.1-10.30-55.RData')
+
+### show trade graphs from stats
 
 tradeGraphs (
 	stats = stats,
