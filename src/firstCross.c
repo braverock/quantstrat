1107b518cf3b9eef10d683108f6efdb50c487b5d
Joshua M. Ulrich <josh.m.ulrich@gmail.com>
2014-05-08 11:08:00 +0000

    - Fix protect stack imbalance
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/quantstrat@1609 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/firstCross.c
+++ src/firstCross.c
@@ -16,42 +16,54 @@ SEXP firstCross(SEXP x, SEXP th, SEXP rel, SEXP start)
     int_rel = asInteger(rel);
     int_start = asInteger(start)-1;
 
+    /* return number of observations if relationship is never TRUE */
+    SEXP result = ScalarInteger(nrows(x));
+
     switch(int_rel) {
         case 1:  /* >  */
             real_x = REAL(x);
             for(i=int_start; i<nrows(x); i++)
-                if(real_x[i] >  real_th)
-                    return(ScalarInteger(i+1));
+                if(real_x[i] >  real_th) {
+                    result = ScalarInteger(i+1);
+                    break;
+                }
             break;
         case 2:  /* <  */
             real_x = REAL(x);
             for(i=int_start; i<nrows(x); i++)
-                if(real_x[i] <  real_th)
-                    return(ScalarInteger(i+1));
+                if(real_x[i] >  real_th) {
+                    result = ScalarInteger(i+1);
+                    break;
+                }
             break;
         case 3:  /* == */
             real_x = REAL(x);
             for(i=int_start; i<nrows(x); i++)
-                if(real_x[i] == real_th)
-                    return(ScalarInteger(i+1));
+                if(real_x[i] >  real_th) {
+                    result = ScalarInteger(i+1);
+                    break;
+                }
             break;
         case 4:  /* >= */
             real_x = REAL(x);
             for(i=int_start; i<nrows(x); i++)
-                if(real_x[i] >= real_th)
-                    return(ScalarInteger(i+1));
+                if(real_x[i] >  real_th) {
+                    result = ScalarInteger(i+1);
+                    break;
+                }
             break;
         case 5:  /* <= */
             real_x = REAL(x);
             for(i=int_start; i<nrows(x); i++)
-                if(real_x[i] <= real_th)
-                    return(ScalarInteger(i+1));
+                if(real_x[i] >  real_th) {
+                    result = ScalarInteger(i+1);
+                    break;
+                }
             break;
         default:
             error("unsupported relationship operator");
   }
   UNPROTECT(P);
-  /* return number of observations if relationship is never TRUE */
-  return(ScalarInteger(nrows(x)));
+  return(result);
 }
 
