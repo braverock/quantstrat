9514d51ea957463fbde71bc7ed783e3f0af75cf2
Joshua M. Ulrich <josh.m.ulrich@gmail.com>
2015-12-11 14:44:38 +0000

    Remove "tests" that don't test anything
    All the test_* files were essentially verbatim copies of the demo
    test template. Therefore, they all failed and prevented any other
    tests from running.
    
    Also remove unnecessary files prepend.sh and (the very dangerous)
    nuke_environments.R.
    
    
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/blotter/pkg/quantstrat@1722 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/tests/nuke_environments.R
+++
@@ -1,4 +0,0 @@
-rm(list=ls(.strategy), pos=.strategy)
-rm(list=ls(.blotter), pos=.blotter)
-rm_instruments( keep.currencies = FALSE)
-rm(list=ls())
--- inst/tests/prepend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for file in *; do
-  cp "$file" "test_$file"
-done
