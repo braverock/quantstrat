62418d31f079c2cefc9f25cb4d28e7fec1044123
Brian G. Peterson <brian@braverock.com>
2017-07-27 09:03:30 +0000

    improve handling of the audit.filename parameter
    - fix and re-enable pattern match for the audit.filename
    
    - allow audit.filename to be an environment for dev/debug
    
    - expand and update documentation
    
    see #61

--- R/chart.forward.training.R
+++ R/chart.forward.training.R
@@ -1,23 +1,42 @@
 #' Chart to analyse walk.forward() objective function
 #'
+#' The \code{\link{walk.forward}} function creates an audit environment, written 
+#' out as a results file, which contains the out of sample results of the (chosen)
+#' parameter set. It also writes out in-sample audit environment files for each 
+#' training period.
+#' 
+#' This function reads one of those training files and will construct a performance 
+#' chart of the in-sample performance of all the parameter sets during the training
+#' period, inbcluding the in-sample performance of the chosen parameter set.
+#' 
+#' Note that parameter \code{audit filename} may also be an audit environment 
+#' which is already loaded in \R, for ease of development and debugging.  Little
+#' checking is done to ensure the correct structure of this environment, so passing
+#' an unsuitable environment (such as the .blotter environment) will result in
+#' errors.
+#' 
 #' @param audit.filename name of .audit environment file as produced by walk.forward().
 #'        Filename will match pattern [audit.prefix].[symbol].[start timestamp].[end timestamp].RData.
 #'
-#' @seealso \code{\link{walk.forward}}
+#' @seealso \code{\link{walk.forward}}, \code{\link{chart.forward}}
 #' @export
 chart.forward.training <- function(audit.filename)
 {
+  if(is.environment(audit.filename)){
+    .audit <- audit.filename
+  } else {
     .audit <- NULL  # keep codetools happy
-    # # ensure correct file written by walk.forward() is provided
-    # datePattern <- "[[:digit:]]{4}.[[:digit:]]{2}.[[:digit:]]{2}(.[[:digit:]]{2}.[[:digit:]]{2}.[[:digit:]]{2})?"
-    # if (!grepl(paste0(datePattern, "\\.", datePattern, "\\.RData$"), audit.filename[1L])) {
-    #     stop("'audit.filename' should match pattern:\n  [audit.prefix].[symbol].[start timestamp].[end timestamp].RData.")
-    # }
-    if (file.exists(audit.filename)) {
-        load(audit.filename)
-    } else {
-        stop("'audit.filename', ", audit.filename, " not found.")
+    # ensure correct training file written by walk.forward() is provided
+    datePattern <- "[[:digit:]]{8}T[[:digit:]]{6}?"
+    if (!grepl(paste0(datePattern, "\\.", datePattern, "\\.RData$"), audit.filename[1L])) {
+      stop("'audit.filename' should match pattern:\n  [audit.prefix].[symbol].[start timestamp].[end timestamp].RData for trainging file written by walk.forward.")
     }
+    if (file.exists(audit.filename)) {
+      load(audit.filename)
+    } else {
+      stop("'audit.filename', ", audit.filename, " not found.")
+    }
+  }
 
     # extract all portfolio names from the audit environment
     # NB: training data only has portfolios that end in digits
