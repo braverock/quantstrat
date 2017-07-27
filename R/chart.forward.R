7e192363101708d03ede6e46cd7a7bf59a8b1cf1
Brian G. Peterson <brian@braverock.com>
2017-07-27 09:03:30 +0000

    improve handling of the audit.filename parameter
    - fix and re-enable pattern match for the audit.filename
    
    - allow audit.filename to be an environment for dev/debug
    
    - expand and update documentation
    
    see #61

--- R/chart.forward.R
+++ R/chart.forward.R
@@ -1,22 +1,40 @@
 #' Chart to analyse walk.forward() objective function
 #'
+#' The \code{\link{walk.forward}} function creates an audit environment, written 
+#' out as a results file, which contains the out of sample results of the (chosen)
+#' parameter set.  This function parses that file and generates a chart to 
+#' compare the performance of all parameter sets against the optimal one. 
+#'
+#' \code{\link{chart.forward.training}} uses the audit environment for a single 
+#' in-sample training period to draw a chart for that in-sample data.
+#' 
+#' Note that parameter \code{audit filename} may also be an audit environment 
+#' which is already loaded in \R, for ease of development and debugging.  Little
+#' checking is done to ensure the correct structure of this environment, so passing
+#' an unsuitable environment (such as the .blotter environment) will result in
+#' errors.
+#' 
 #' @param audit.filename name of .audit environment file as produced by walk.forward().
 #'        Filename will match pattern [audit.prefix].results.RData.
 #' 
-#' @seealso \code{\link{walk.forward}}
+#' @seealso \code{\link{walk.forward}}, \code{\link{chart.forward.training}}
 #' @export
 chart.forward <- function(audit.filename)
 {
+  if(is.environment(audit.filename)){
+    .audit <- audit.filename
+  } else {
     .audit <- NULL  # keep codetools happy
-    # # ensure correct file written by walk.forward() is provided
-    # if (!grepl("\\.results\\.RData$", audit.filename[1L])) {
-    #     stop("'audit.filename' should match pattern:\n  [audit.prefix].results.RData")
-    # }
+    # ensure correct file written by walk.forward() is provided
+    if (!grepl("\\.results\\.RData$", audit.filename[1L])) {
+       stop("'audit.filename' should match pattern:\n  [audit.prefix].results.RData")
+    }
     if (file.exists(audit.filename)) {
         load(audit.filename)
     } else {
         stop("'audit.filename', ", audit.filename, " not found.")
     }
+  }
 
     # extract all portfolio names from the audit environment,
     # except result portfolio (which doesn't end with a digit)
